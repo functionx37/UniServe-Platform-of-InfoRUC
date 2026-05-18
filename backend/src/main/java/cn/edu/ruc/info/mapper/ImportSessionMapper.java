c0374399e79f1d140c45bee776218662b8ee8059
shaodao13 <3267752353@qq.com>
2026-05-18 10:11:22 +0800

    feat: 创建全部 Mapper 接口（13个），实现 MyBatis-Plus 基础映射

---
+++ backend/src/main/java/cn/edu/ruc/info/mapper/ImportSessionMapper.java
@@ -0,0 +1,9 @@
+package cn.edu.ruc.info.mapper;
+
+import cn.edu.ruc.info.entity.ImportSession;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ImportSessionMapper extends BaseMapper<ImportSession> {
+}
---
+++ backend/src/main/java/cn/edu/ruc/info/mapper/PartyStageMapper.java
@@ -0,0 +1,9 @@
+package cn.edu.ruc.info.mapper;
+
+import cn.edu.ruc.info.entity.PartyStage;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface PartyStageMapper extends BaseMapper<PartyStage> {
+}

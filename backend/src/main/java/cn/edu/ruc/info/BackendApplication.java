7484fe7720e62a4f4c2b1000a97a225eec49f2bf
shaodao13 <3267752353@qq.com>
2026-05-18 11:02:02 +0800

    feat: 完善启动类，添加 MapperScan 扫描配置以支持数据库操作

--- backend/src/main/java/cn/edu/ruc/info/BackendApplication.java
+++ backend/src/main/java/cn/edu/ruc/info/BackendApplication.java
@@ -1,9 +1,11 @@
 package cn.edu.ruc.info;
 
+import org.mybatis.spring.annotation.MapperScan; // 必须新增这一行
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("cn.edu.ruc.info.mapper") // 必须新增这一行，指向你存放 Mapper 的包
 public class BackendApplication {
 
 	public static void main(String[] args) {
